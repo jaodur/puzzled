5e110a248904d38d0a46b6d913be8769f878a535
ja-odur <39955305+ja-odur@users.noreply.github.com>
2019-06-14 18:59:29 +0300

    chore(django-react): configure react django  - configure react django [Maintains #166702498]

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+var webpack = require('webpack');
+var BundleTracker = require('webpack-bundle-tracker');
+
+module.exports = {
+    context: __dirname,
+
+    entry:'./puzzled/apps/puzzled_front/static/js/index.js',
+
+    output: {
+        path: path.resolve('./puzzled/apps/puzzled_front/static/bundles/'),
+        filename: '[name]-[hash].js'
+    },
+
+    plugins: [
+        new BundleTracker({filename: './webpack-stats.json'})
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    }
+
+};
