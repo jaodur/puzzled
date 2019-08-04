7b33c9878b670fc440cab2773a26fe265999fb2e
odur <odurjoseph8@gmail.com>
2019-08-04 16:14:57 +0300

    ch(sudokuGrid): add highlighting support for errors (duplicate values)
     - add highlighting support for errors (duplicate values)
    
    [Maintains #167565528]

--- puzzled_ui/components/sudoku/sudokuGrid.tsx
+++ puzzled_ui/components/sudoku/sudokuGrid.tsx
@@ -16,6 +16,8 @@ function SudokuGrid({ type }: gridInterface) {
     const [originalPuzzle, setOriginalPuzzle] = React.useState(createDefaultPuzzle(gridState.gridNums));
     const [errors, setErrors] = React.useState(createDefaultPuzzle(gridState.gridNums));
 
+    const [errorFields, setErrorFields] = React.useState([]);
+
     let sudokuGridClass: string = `sudoku-grid-${gridState.type}`;
 
     function createDefaultPuzzle(gridNum: number){
@@ -27,6 +29,15 @@ function SudokuGrid({ type }: gridInterface) {
         return finalArray;
     }
 
+    function createPrefilledArray(coords: Array<Array<number>>, fillValue: number) {
+        let arr: Array<Array<number>> = createDefaultPuzzle(gridState.gridNums);
+        coords.map(([row, col]) => {
+            arr[row][col] = fillValue
+        });
+
+        return arr
+    }
+
     function getGridNums(number?: number): number {
         if (number) {
             return number != 1 ?  number * number : 2;
@@ -35,23 +46,28 @@ function SudokuGrid({ type }: gridInterface) {
     }
 
     function highlightSimilarGrids(row: number, col: number) {
+        function fillCell(arr: any, row: number, col: number, value: number){
+            if(arr[row][col] !== -2){
+                arr[row][col] = value
+            }
+
+        }
         let rowStart: number = Math.floor(row / gridState.type) * gridState.type;
         let colStart: number = Math.floor(col / gridState.type) * gridState.type;
 
-        let coords = createDefaultPuzzle(gridState.gridNums);
+        let coords = createPrefilledArray(errorFields, -2);
 
         for(let row=rowStart; row < rowStart + parseInt(`${gridState.type}`, baseTenRadix); row++){
 
             for(let col=colStart; col < colStart + parseInt(`${gridState.type}`, baseTenRadix); col++){
-                coords[row][col] = -1
+                fillCell(coords, row, col, -1);
             }
         }
 
         for(let i=0; i < gridState.gridNums; i++){
-            coords[row][i] = -1;
-            coords[i][col] = -1;
+            fillCell(coords, row, i, -1);
+            fillCell(coords, i, col, -1);
         }
-        coords[row][col] = 0;
 
         return coords
 
@@ -59,10 +75,11 @@ function SudokuGrid({ type }: gridInterface) {
 
 
     function validateInput(row: number, col: number, inputNum: number){
-        let rowStart: number = Math.floor(row / gridState.type) * gridState.type;
-        let colStart: number = Math.floor(col / gridState.type) * gridState.type;
 
-        function getInnerGrid() {
+
+        function getInnerGrid(row: number, col: number) {
+            let rowStart: number = Math.floor(row / gridState.type) * gridState.type;
+            let colStart: number = Math.floor(col / gridState.type) * gridState.type;
 
             return _.flattenDeep(
                 Array.from(
@@ -73,24 +90,106 @@ function SudokuGrid({ type }: gridInterface) {
             );
         }
 
-        function getAllRelatedGridValues(row: number, col: number, inputNum: number){
+        function getAllRelatedGridValues(row: number, col: number){
             let colArr: number[][] = puzzle.map(innerArr => innerArr.slice(col, col+1));
             highlightSimilarGrids(row, col);
             return _.concat(
-                getInnerGrid(),
+                getInnerGrid(row, col),
                 puzzle[row],
                 _.flattenDeep(colArr)
             )
         }
 
-        if(inputNum >= 0 && inputNum <= gridState.gridNums){
+        function getNumCoord(row: number, col:number, arr: any, inputNum: number) {
+            let rowStart: number = Math.floor(row / gridState.type) * gridState.type;
+            let colStart: number = Math.floor(col / gridState.type) * gridState.type;
+            let [ firstSlice, secondSlice, thirdSlice ] = [
+                gridState.gridNums,
+                2 * gridState.gridNums,
+                3 * gridState.gridNums
+            ];
+            let [ sectorArr, rowArr, colArr ] = [
+                _.slice(arr, 0, firstSlice),
+                _.slice(arr, firstSlice, secondSlice),
+                _.slice(arr , secondSlice, thirdSlice)
+            ];
 
-            if(!getAllRelatedGridValues(row, col, inputNum).includes(inputNum)){
-                return inputNum
+            let coords = [];
+
+            for(let i=0; i < gridState.gridNums; i++){
+                if(inputNum !== 0 && inputNum === sectorArr[i]){
+                    coords.push([ Math.floor(rowStart + (i / gridState.type)), colStart + (i % gridState.type) ])
+                }
+                if(inputNum !== 0 && inputNum === rowArr[i]){
+                    coords.push([row, i])
+                }
+                if(inputNum !== 0 && inputNum === colArr[i]){
+                    coords.push([i, col])
+                }
+
             }
 
+            return coords
         }
-        return 0
+
+        function uniqueArray(arr: Array<Array<number>>) {
+            function uniqueOnly([row, col]: Array<number>){
+                return `${row}${col}`;
+            }
+            return _.uniqBy(arr, uniqueOnly)
+        }
+
+        function removeFromArray(arr: Array<Array<number>>, item: Array<number>){
+            let [row, col] = item;
+            _.remove(arr, ([x, y]) => x === row && y === col);
+        }
+
+
+
+
+        let newErrorCoords = uniqueArray(
+                getNumCoord(row, col, getAllRelatedGridValues(row, col), inputNum)
+        );
+        let newErrorFields = Array.from(errorFields);
+
+        if(Array.isArray(newErrorCoords) && newErrorCoords.length > 0) {
+            newErrorCoords.push([row, col]);
+
+        }else{
+            newErrorCoords = uniqueArray(
+                getNumCoord(row, col, getAllRelatedGridValues(row, col), puzzle[row][col])
+            );
+
+            removeFromArray(newErrorCoords, [row, col]);
+            removeFromArray(newErrorFields, [row, col]);
+
+            Array.from(newErrorCoords).map( arr => {
+
+                let [x, y] = arr;
+
+                let nestedErrors = uniqueArray(
+                    getNumCoord(x, y, getAllRelatedGridValues(x, y), puzzle[x][y])
+                );
+
+                removeFromArray(nestedErrors, [row, col]);
+
+                if(nestedErrors.length == 1){
+
+                    removeFromArray(newErrorCoords, [x, y]);
+                    removeFromArray(newErrorFields, [x, y]);
+                }
+
+            })
+
+        }
+
+        newErrorFields.push(...newErrorCoords);
+        newErrorFields = uniqueArray(newErrorFields);
+
+        setErrorFields(newErrorFields);
+        setErrors(createPrefilledArray(newErrorFields, -2));
+
+        return inputNum
     }
     function concatenateNumbers(prevNum: number, newNum:string, keyCode: number) {
         if(keyCode === deleteKeyCode){
@@ -109,6 +208,11 @@ function SudokuGrid({ type }: gridInterface) {
     }
 
         function decorateFilledInputValue(className: string, row: number, col: number) {
+
+            if(errors[row][col] === -2) {
+                return `${className}__error`;
+
+            }
 
             if(errors[row][col] === -1) {
                 return `${className}__grouped_grid`;
@@ -150,6 +254,7 @@ function SudokuGrid({ type }: gridInterface) {
         let newType: number = event.target.value;
         setPuzzle(createDefaultPuzzle(getGridNums(newType)));
         setOriginalPuzzle(createDefaultPuzzle(getGridNums(newType)));
+        setErrorFields([]);
         setErrors(createDefaultPuzzle(getGridNums(newType)));
         changeGridState({type: newType, gridNums: getGridNums(newType)});
     }
@@ -158,6 +263,7 @@ function SudokuGrid({ type }: gridInterface) {
 
         return function keyDown(event: eventInterface) {
             event.preventDefault();
+            setErrors(highlightSimilarGrids(row, col));
             setPuzzle(updatePuzzleValue(row, col, event.key, event.keyCode));
             setOriginalPuzzle(puzzle);
         }
