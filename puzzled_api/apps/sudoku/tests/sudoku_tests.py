fba00944b3e1bb19ae307ba06d6dd2f89e34ac5f
odur <odurjoseph8@gmail.com>
2019-08-29 20:02:07 +0300

    ft(generatePuzzle): add tests
     - add tests
    
    [Delivers #168076414]

--- puzzled_api/apps/sudoku/tests/sudoku_tests.py
+++ puzzled_api/apps/sudoku/tests/sudoku_tests.py
@@ -1,6 +1,14 @@
 from ..game_logic import Sudoku
 import pytest
-from .fixtures import invalid_puzzle
+from .fixtures import (
+    puzzle,
+    invalid_puzzle,
+    expert_prefilled,
+    expert_coords,
+    hard_coords,
+    medium_coords,
+    str_representation
+)
 
 
 class TestSudoku:
@@ -8,3 +16,32 @@ class TestSudoku:
     def test_invalid_puzzle_fails(self, invalid_puzzle):
         with pytest.raises(Exception) as e:
             Sudoku(invalid_puzzle, 3)
+
+    def test_difficulty_pattern_generator_expert_succeeds(self, expert_prefilled, expert_coords):
+        prefilled, coords = Sudoku.difficulty_pattern_generator(3, 'expert')
+
+        assert prefilled == expert_prefilled
+        assert coords == expert_coords
+
+    def test_difficulty_pattern_generator_hard_succeeds(self, hard_coords):
+        prefilled, coords = Sudoku.difficulty_pattern_generator(3, 'hard')
+
+        assert prefilled == []
+        assert coords == hard_coords
+
+    def test_difficulty_pattern_generator_medium_succeeds(self, medium_coords):
+        prefilled, coords = Sudoku.difficulty_pattern_generator(3, 'medium')
+
+        assert prefilled == []
+        assert coords == medium_coords
+
+    def test_string_representation(self, puzzle, str_representation):
+
+        str_rep = str(Sudoku(puzzle, 3))
+
+        assert str_representation in str_rep
+
+    def test_repr_representation(self, puzzle):
+        repr_rep = repr(Sudoku(puzzle, 3))
+
+        assert 'type=3' in repr_rep
