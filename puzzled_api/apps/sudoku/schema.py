4a67d88abbc96698233543351e19ba125082d8a9
odur <odurjoseph8@gmail.com>
2019-09-10 16:47:32 +0300

    ft(generatePuzzle): add generatePuzzle mutation
     - add generatePuzzle mutation
    
    [Delivers #168403596]

--- puzzled_api/apps/sudoku/schema.py
+++ puzzled_api/apps/sudoku/schema.py
@@ -29,7 +29,21 @@ class SolveSudoku(graphene.Mutation):
 
         return SolveSudoku(puzzle=sudoku.puzzle, p_type=p_type)
 
+class GenerateSudoku(graphene.Mutation):
+    puzzle = graphene.List(graphene.List(graphene.Int))
+    difficulty = graphene.String()
+    p_type = graphene.Int()
+
+    class Arguments:
+        difficulty = graphene.String()
+        p_type = graphene.Int()
+
+    def mutate(self, info, difficulty, p_type):
+        sudoku = Sudoku.generate(puzzle_type=p_type, difficulty=difficulty)
+        return GenerateSudoku(puzzle=sudoku.puzzle, difficulty=difficulty, p_type=p_type)
+
 
 class Mutation(graphene.ObjectType):
     solve_sudoku = SolveSudoku.Field()
+    generate_sudoku  = GenerateSudoku.Field()
 
