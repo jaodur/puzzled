fced5b208ad1b50e8dde30b828a1360293d5fa7c
odur <odurjoseph8@gmail.com>
2019-10-07 22:52:53 +0300

    chore(linting): add test-py invoke command
     - add test-py invoke command
    
    [Maintains #168968734]

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,11 +1,12 @@
 from invoke import Collection
 
-from .testing_tasks import lint_js
+from .testing_tasks import lint_js, test_python
 from .dev_tasks import prettier, set_node_path, install_requirements
 
 task_col = Collection()
 
 task_col.add_task(prettier)
 task_col.add_task(lint_js)
+task_col.add_task(test_python)
 task_col.add_task(set_node_path)
 task_col.add_task(install_requirements)
--- tasks/testing_tasks.py
+++ tasks/testing_tasks.py
@@ -6,3 +6,8 @@ def lint_js(context):
     """lint the javascript files"""
     run('bin/tests/check_prettier_errors.sh --all', echo=True)
     run("eslint --report-unused-disable-directives --ext .js,.jsx,.ts,.tsx frontend", echo=True)
+
+
+@task(name='test-py')
+def test_python(context):
+    run('pytest --cov=backend/ && coverage report', echo=True)
