59e2dc0a91e6d661ba8a606e3397d078aa9784f2
odur <odurjoseph8@gmail.com>
2019-10-07 15:54:36 +0300

    chore(linting): override linting rules
     - override linting rules
    
    [Maintains #168968734]

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,12 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  extends: ['plugin:@typescript-eslint/recommended', "plugin:prettier/recommended"],
+  extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  rules: {
+        '@typescript-eslint/explicit-function-return-type': 0,
+        '@typescript-eslint/no-empty-function': 0,
+        '@typescript-eslint/no-inferrable-types': 0,
+        '@typescript-eslint/no-explicit-any': 0,
+        '@typescript-eslint/no-use-before-define': 0,
+  },
 };
