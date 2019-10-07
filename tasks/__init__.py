c3e81af188322cfe9075baf5c2d42a75baacdbe8
odur <odurjoseph8@gmail.com>
2019-10-07 22:16:41 +0300

    chore(linting): add node_path invoke command
     - add node_path invoke command
    
    [Maintains #168968734]

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,9 +1,10 @@
 from invoke import Collection
 
 from .testing_tasks import lint_js
-from .dev_tasks import prettier
+from .dev_tasks import prettier, set_node_path
 
 task_col = Collection()
 
 task_col.add_task(prettier)
 task_col.add_task(lint_js)
+task_col.add_task(set_node_path)
--- tasks/dev_tasks.py
+++ tasks/dev_tasks.py
@@ -1,6 +1,15 @@
 from invoke import run, task
 
+
 @task(name='prettier')
 def prettier(context):
     """Runs prettier, a JS code formatter"""
     run('bin/prettier_fix.sh', echo=True)
+
+
+@task(name='set-node-path')
+def set_node_path(context):
+    """set node path"""
+    run('bin/functions/set_node_path.sh', echo=True)
+
+
