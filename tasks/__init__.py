54cbf87a2c4399a948c7b0bf7b24f7e29a958c85
odur <odurjoseph8@gmail.com>
2019-10-12 23:22:38 +0300

    chore(linting): add autopep8 fixing
     - add autopep8 fixing
    
    [Maintains #168968734]

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,11 +1,12 @@
 from invoke import Collection
 
 from .testing_tasks import lint_js, lint_py, test_python
-from .dev_tasks import prettier, set_node_path, install_requirements
+from .dev_tasks import prettier_js, prettier_py, set_node_path, install_requirements
 
 task_col = Collection()
 
-task_col.add_task(prettier)
+task_col.add_task(prettier_js)
+task_col.add_task(prettier_py)
 task_col.add_task(lint_js)
 task_col.add_task(lint_py)
 task_col.add_task(test_python)
--- tasks/dev_tasks.py
+++ tasks/dev_tasks.py
@@ -1,10 +1,14 @@
 from invoke import run, task
 
 
-@task(name='prettier')
-def prettier(context):
+@task(name='prettier-pj')
+def prettier_js(context):
     """Runs prettier, a JS code formatter"""
     run('bin/prettier_fix.sh', echo=True)
+
+@task(name='prettier-py')
+def prettier_py(context):
+    run('autopep8 --in-place --recursive .', echo=True)
 
 
 @task(name='set-node-path')
