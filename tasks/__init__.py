dbe77f83232550a37d784b7c63990c17a2b68790
odur <odurjoseph8@gmail.com>
2019-10-12 22:48:23 +0300

    chore(linting): add python linting
     - add python linting
    
    [Maintains #168968734]

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,12 +1,13 @@
 from invoke import Collection
 
-from .testing_tasks import lint_js, test_python
+from .testing_tasks import lint_js, lint_py, test_python
 from .dev_tasks import prettier, set_node_path, install_requirements
 
 task_col = Collection()
 
 task_col.add_task(prettier)
 task_col.add_task(lint_js)
+task_col.add_task(lint_py)
 task_col.add_task(test_python)
 task_col.add_task(set_node_path)
 task_col.add_task(install_requirements)
--- tasks/testing_tasks.py
+++ tasks/testing_tasks.py
@@ -7,6 +7,10 @@ def lint_js(context):
     run('bin/tests/check_prettier_errors.sh --all', echo=True)
     run("eslint --report-unused-disable-directives --ext .js,.jsx,.ts,.tsx frontend", echo=True)
 
+@task(name='lint-py')
+def lint_py(context):
+    run('flake8 --config=.flake8 *.py .', echo=True)
+
 
 @task(name='test-py')
 def test_python(context):
