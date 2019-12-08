20f5a4d74c25dfd9022e9e07ea396f818416a80b
odur <odurjoseph8@gmail.com>
2019-12-09 00:28:37 +0300

    ft: add picture_url validation
     - add picture_url validation
    
    [Delivers #170035667]

--- backend/apps/authentication/models.py
+++ backend/apps/authentication/models.py
@@ -1,28 +1,11 @@
-import re
 from django.db import models, transaction
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import PermissionsMixin
 from django.utils.translation import ugettext_lazy as _
 import pytz
 
-mailbox_pat = r"[-!#$%&'*+/=?^_`{}|~0-9A-Z]+(\.[-!#$%&'*+/=?^_`{}|~0-9A-Z]+)*"  # dot-atom
-
-# max length of the domain is 249: 254 (max email length) minus one
-# period, two characters for the TLD, @ sign, & one character before @.
-domain_pat = r'(?:[A-Z0-9](?:[A-Z0-9-]{0,247}[A-Z0-9])?\.)+(?:[A-Z]{2,63})'
-email_pat = r'@'.join((mailbox_pat, domain_pat))
-
-# Compile the complete email regular expression
-email_re = re.compile(r'^' + email_pat + r'$', re.IGNORECASE)
-
-
-def validate_email(email):
-    """
-    Performs a few checks on the given email address to filter out any email
-    addresses we don't want to record in our address books (most likely email
-    addresses that are invalid, or belong to robots).
-    """
-    return bool(email_re.match(email))
+from backend.lib.exceptions import FieldValidationError
+from backend.lib.validators import validate_email, url_validator
 
 
 class UserManager(BaseUserManager):
@@ -33,11 +16,20 @@ class UserManager(BaseUserManager):
         Creates and saves a User with the given email and password.
 
         """
+        picture_url = extra_fields.pop('picture_url', '').strip()
+
         if not email:
             raise ValueError('The given email must be set')
         if not validate_email(email):
-            raise ValueError('Not a valid email address: {}'.format(email))
+            raise FieldValidationError('Not a valid email address', 'email', email)
         email = self.normalize_email(email)
+
+        if picture_url:
+            try:
+                url_validator(picture_url)
+            except Exception as e:
+                raise FieldValidationError('Not a valid url', 'picture_url', picture_url)
+
 
         with transaction.atomic(using=self._db):
             user = self.model(email=email, **extra_fields)
