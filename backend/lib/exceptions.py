094176586d56d3698f60eb647436da7ac284e1ae
odur <odurjoseph8@gmail.com>
2019-12-09 00:25:20 +0300

    ft: add utils and custom exceptions
     - add utils and custom exceptions
    
    [Delivers #170035667]

---
+++ backend/lib/exceptions.py
@@ -0,0 +1,15 @@
+class FieldValidationError(Exception):
+
+    def __init__(self, message, field, params, code='required'):
+        super().__init__(message)
+        self.field = field
+        self.message = message
+        self.params = params
+        self.code = code
+
+
+class GraphQLValidationError(Exception):
+
+    def __init__(self, message, errors):
+        super().__init__(message)
+        self.errors = errors
