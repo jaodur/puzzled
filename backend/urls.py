549e4a20d2aaa5fbe2992fa78ea15b945ce719d9
odur <odurjoseph8@gmail.com>
2019-12-09 00:26:18 +0300

    ft: add base graphql view
     - add base graphql view
    
    [Delivers #170035667]

--- backend/urls.py
+++ backend/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.decorators.csrf import csrf_exempt
-from graphene_django.views import GraphQLView
+from backend.lib.base import BaseGraphQLView
 from .schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
+    path('graphql/', csrf_exempt(BaseGraphQLView.as_view(graphiql=True, schema=schema))),
     path('', include('frontend.urls')),
 
 ]
