de24da94e2ce4c251b5410f1462a20fa634db371
odur <odurjoseph8@gmail.com>
2019-12-14 15:07:38 +0300

    ch: add webpack-dev-server config
     - add webpack-dev-server config
    
    [Maintains #170289642]

---
+++ webpack_dev_server.config.js
@@ -0,0 +1,39 @@
+var path = require("path");
+var webpack = require('webpack');
+var BundleTracker = require('webpack-bundle-tracker');
+
+var config = require('./webpack.base.config.js');
+
+config.devServer = {
+        port: 3000,
+        headers: { 'Access-Control-Allow-Origin': '*' },
+        compress: true,
+        hot: true,
+    };
+
+// use webpack-dev-server
+config.entry = [
+        'webpack-hot-middleware/client?http://localhost:3000/',
+        './frontend/index.tsx',
+    ];
+
+// override django's STATIC_URL for webpack bundles
+config.output.publicPath = 'http://localhost:3000/frontend/static/bundles/';
+
+// Add HotModuleReplacementPlugin and BundleTracker plugins
+config.plugins =  config.plugins.concat([
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),
+        new BundleTracker({filename: './webpack-stats.json'}),
+    ]);
+
+// Add a loader for JSX files with react-hot enabled
+config.module.rules.push(
+  {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    use: ['react-hot', 'babel-loader']
+  }
+);
+
+module.exports = config;
