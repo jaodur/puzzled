703d835e3981ab669f7832c64182c598d7918490
odur <odurjoseph8@gmail.com>
2019-12-15 19:11:51 +0300

    ch: add invoke commands for webpack-server, local-builds and python-server
     - add invoke commands for webpack-server, local-builds and python-server
    
    [Maintains #170289642]

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,7 +1,7 @@
 from invoke import Collection
 
 from .testing_tasks import lint_js, lint_py, test_python
-from .dev_tasks import prettier_js, prettier_py, set_node_path, install_requirements
+from .dev_tasks import prettier_js, prettier_py, set_node_path, install_requirements, build_local, webpack_dev_server, python_server
 
 task_col = Collection()
 
@@ -12,3 +12,6 @@ task_col.add_task(lint_py)
 task_col.add_task(test_python)
 task_col.add_task(set_node_path)
 task_col.add_task(install_requirements)
+task_col.add_task(build_local)
+task_col.add_task(webpack_dev_server)
+task_col.add_task(python_server)
--- tasks/dev_tasks.py
+++ tasks/dev_tasks.py
@@ -23,3 +23,22 @@ def install_requirements(context):
     """Install python and node requirements"""
     run('pipenv install --skip-lock', echo=True)
     run('npm install', echo=True)
+
+
+@task(name='build-local')
+def build_local(context):
+    """run webpack build"""
+    run('npm run build', echo=True)
+
+
+@task(name='webpack-server')
+def webpack_dev_server(context):
+    """run webpack-dev server"""
+    run('npm run watch', echo=True)
+
+
+@task(name='run-server')
+def python_server(context):
+    """run python server"""
+    run('python manage.py runserver', echo=True)
+
