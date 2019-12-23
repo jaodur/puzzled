4e0fbf3affe33f21a593b2ddbd99084761ab7a89
odur <odurjoseph8@gmail.com>
2019-12-24 00:19:59 +0300

    ft: add schema fixtures and tests
     - add schema fixtures and tests
    
    [Delivers #170035667]

--- backend/apps/authentication/tests/fixtures.py
+++ backend/apps/authentication/tests/fixtures.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
 import faker
+from graphene.test import Client
+from backend.schema import schema
 import pytest
 
 FAKE = faker.Faker()
@@ -23,3 +25,35 @@ def user_data():
         'password': FAKE.password(),
         'picture_url': FAKE.url()
     }
+
+
+def create_user_mutation():
+    return (
+        '''
+        mutation {
+            createUser(firstName:"test", lastName: "test", email:"test@gmail.com", password: "test1234"){
+                user{
+                    id,
+                    name,
+                    email
+                }
+            }
+        }
+        '''
+    )
+
+
+def create_user_invalid_email_mutation():
+    return (
+        '''
+        mutation {
+            createUser(firstName:"test", lastName: "test", email:"invalid_email", password: "test1234"){
+                user{
+                    id,
+                    name,
+                    email
+                }
+            }
+        }
+        '''
+    )
