9012a90d46b42ff9ccd7ece0308e41848b2dba09
odur <odurjoseph8@gmail.com>
2019-12-23 15:10:36 +0300

    ft: add fixtures
     - add fixtures
    
    [Delivers #170035667]

---
+++ backend/apps/authentication/tests/fixtures.py
@@ -0,0 +1,25 @@
+from django.contrib.auth import get_user_model
+import faker
+import pytest
+
+FAKE = faker.Faker()
+
+
+@pytest.fixture(scope='module')
+def user():
+    return get_user_model()(
+       name=FAKE.first_name(),
+       preferred_name=FAKE.first_name(),
+       email=FAKE.email()
+    )
+
+
+@pytest.fixture(scope='function')
+def user_data():
+    return {
+        'first_name': FAKE.first_name(),
+        'last_name': FAKE.last_name(),
+        'email': FAKE.email(),
+        'password': FAKE.password(),
+        'picture_url': FAKE.url()
+    }
