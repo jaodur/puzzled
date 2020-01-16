7160da148dfdcbf3d3d7b1234b27f6de795d338e
odur <odurjoseph8@gmail.com>
2020-01-16 16:15:15 +0300

    ft: remove unused imports
     - remove unused imports
    
    [Delivers #170465019]

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -9,7 +9,7 @@ import './styles/snackbar.sass';
 import { PageNotFound } from './components/commons/404PageNotFound';
 import { Home } from './components/homePage/Home';
 import { SudokuHome } from './components/sudoku/sudoku';
-import { SignUp } from './components/profile/siginup'
+import SignUp from './components/profile/siginup'
 
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloClient } from 'apollo-client';
