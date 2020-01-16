2a7cd1cc2e27f56c5ac71eed70782b027f46b78c
odur <odurjoseph8@gmail.com>
2020-01-16 16:12:57 +0300

    ft: add userLogin validators
     - add userLogin validators
    
    [Delivers #170465019]

---
+++ frontend/components/validators/authentication.ts
@@ -0,0 +1,17 @@
+import { emailFormat } from "./common";
+
+const userLogInConstraints = {
+    email: {
+        presence: true,
+        format: emailFormat,
+    },
+    password: {
+        presence: true,
+        length: {
+            minimum: 4,
+            message: "Must be at least 6 characters."
+        }
+    }
+};
+
+export { userLogInConstraints }
---
+++ frontend/components/validators/common.ts
@@ -0,0 +1,8 @@
+
+const emailFormat = {
+    pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    message: 'Please provide a valid email address.'
+}
+
+export { emailFormat }
+
