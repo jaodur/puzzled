70911bdb00d2fef9b18dcef8ca34768f880d178d
odur <odurjoseph8@gmail.com>
2020-02-04 19:32:04 +0300

    ft: update checkLogin to include setLoginInfo
     - update checkLogin to include setLoginInfo
    
    [Delivers #170843732]

--- frontend/lib/session/checkLogin.ts
+++ frontend/lib/session/checkLogin.ts
@@ -1,25 +1,39 @@
 import { CHECK_LOGIN_MUTATION } from '../../graphql/mutations/authentication';
 import { graphqlMutate } from '../api/graphqlHttp';
 
+interface LoginInfoInterface {
+    loggedIn: boolean;
+    user?: {
+        name: string;
+        preferredName: string;
+        email: string;
+        telephone?: string;
+        pictureUrl?: string;
+        timezone?: string;
+    };
+}
+
 class CheckLogin {
-    public _loginInfo: object;
+    public _loginInfo: LoginInfoInterface;
 
     public async checkLogin() {
         return await graphqlMutate(CHECK_LOGIN_MUTATION);
     }
 
+    public async setLoginInfo(newLoginInfo: LoginInfoInterface) {
+        this._loginInfo = newLoginInfo;
+    }
+
     public async getLoginInfo() {
         if (!this._loginInfo) {
-            const loginInfo = await this.checkLogin();
-
-            if (loginInfo.loggedIn) {
-                this._loginInfo = loginInfo;
-            }
-
-            return loginInfo;
+            this._loginInfo = await this.checkLogin().then(data => data.checkLogin);
         }
         return this._loginInfo;
     }
 }
 
-export default new CheckLogin();
+const checkLoginInstance = new CheckLogin();
+
+checkLoginInstance.getLoginInfo();
+
+export default checkLoginInstance;
