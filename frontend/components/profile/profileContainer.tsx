c83131bd3ce0fcd8218503178d879e1300827d19
odur <odurjoseph8@gmail.com>
2020-02-07 22:34:49 +0300

    ft: add redirect to login page if user is anonymous
     - add redirect to login page if user is anonymous
    
    [Delivers #170843732]

--- frontend/components/profile/profileContainer.tsx
+++ frontend/components/profile/profileContainer.tsx
@@ -1,13 +1,18 @@
 import * as React from 'react';
+import { Redirect } from 'react-router-dom';
 
 import { Footer } from '../commons/footer';
+import { links } from '../commons/linkUrls';
 import { NavBarContainer } from '../commons/navbarContainer';
+import { useCheckLoginContext } from '../commons/puzzleContext';
 import { Profile } from './profile';
 
 const footerClass: string = 'main-footer';
 
 function ProfileContainer() {
-    return (
+    const { checkLogin } = useCheckLoginContext();
+
+    return checkLogin._loginInfo.loggedIn ? (
         <>
             <NavBarContainer styleClass={'default-navbar-container'} />
             <div className={'default-nav-strip'} />
@@ -16,6 +21,8 @@ function ProfileContainer() {
             </div>
             <Footer footerClass={footerClass} key={'sudoku-footer'} />
         </>
+    ) : (
+        <Redirect to={links.USER.SIGN_IN} />
     );
 }
 
