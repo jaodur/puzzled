f6d9b7f9d1af593b1252bdda2c3fb1c1f51dd309
odur <odurjoseph8@gmail.com>
2020-02-10 00:55:12 +0300

    ft: add state to changePassword component
     - add state to changePassword component
    
    [Delivers #170843732]

--- frontend/components/profile/changePassword.tsx
+++ frontend/components/profile/changePassword.tsx
@@ -2,11 +2,54 @@ import * as React from 'react';
 
 import Typography from '@material-ui/core/Typography';
 
+import { validate } from 'validate.js';
+import { deepCopy } from '../../utils/utils';
 import { Button } from '../commons/button';
 import { StackedInput } from '../commons/inputs';
+import { EventInterface } from '../interfaces/interfaces';
 import { ChangePasswordInterface } from '../interfaces/profile';
+import { changePasswordConstraints } from '../validators/authentication';
 
 function ChangePassword({ styleClass, themeStyleClass }: ChangePasswordInterface) {
+    const preventDefault = (event: any) => event.preventDefault();
+    const [password, setPassword] = React.useState(changePasswordInitState());
+    const [passwordErrors, setPasswordErrors] = React.useState(changePasswordInitState());
+
+    function changePasswordInitState() {
+        return {
+            password: '',
+            newPassword: '',
+            confirmPassword: '',
+        };
+    }
+
+    function validateUserInputs(userInputs: object, constraints: object, fullMessages: boolean = false) {
+        return validate(userInputs, constraints, { fullMessages });
+    }
+
+    function onPasswordFieldChange(key: string) {
+        return function(event: EventInterface) {
+            preventDefault(event);
+
+            const updatedPasswordInfo = deepCopy(password);
+            updatedPasswordInfo[key] = event.target.value;
+            let errors = validateUserInputs(updatedPasswordInfo, changePasswordConstraints);
+
+            errors = errors || {};
+
+            if (updatedPasswordInfo.confirmPassword !== updatedPasswordInfo.newPassword) {
+                errors = { ...errors, confirmPassword: ["Password doesn't match"] };
+            }
+
+            setPasswordErrors(errors);
+            setPassword(updatedPasswordInfo);
+        };
+    }
+
+    function checkEmpty(value: string) {
+        return !!value ? value[0] : '';
+    }
+
     return (
         <div>
             <div style={{ backgroundColor: 'white', padding: '5%' }}>
@@ -15,9 +58,26 @@ function ChangePassword({ styleClass, themeStyleClass }: ChangePasswordInterface
                 </Typography>
 
                 <div className={themeStyleClass.root}>
-                    <StackedInput label={'Current Password'} defaultValue={''} type={'password'}/>
-                    <StackedInput label={'New Password'} defaultValue={''} type={'password'}/>
-                    <StackedInput label={'Confirm Password'} defaultValue={''} type={'password'}/>
+                    <StackedInput
+                        label={'Current Password'}
+                        defaultValue={''}
+                        type={'password'}
+                        onChange={onPasswordFieldChange('password')}
+                    />
+                    <StackedInput
+                        label={'New Password'}
+                        defaultValue={''}
+                        type={'password'}
+                        onChange={onPasswordFieldChange('newPassword')}
+                        errorMsg={checkEmpty(passwordErrors.newPassword)}
+                    />
+                    <StackedInput
+                        label={'Confirm Password'}
+                        defaultValue={''}
+                        type={'password'}
+                        onChange={onPasswordFieldChange('confirmPassword')}
+                        errorMsg={checkEmpty(passwordErrors.confirmPassword)}
+                    />
                 </div>
                 <div className={`${styleClass}__align_right`}>
                     <Button label={'cancel'} styleClass={'cancel-btn'} />
