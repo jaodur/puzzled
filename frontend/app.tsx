a3544cfe76057649dbea5eb8af1e6651194d4484
odur <odurjoseph8@gmail.com>
2020-02-11 22:54:16 +0300

    ft: fix routing issue caused by react-router
     - fix routing issue caused by react-router
    
    [Delivers #170843732]

--- frontend/app.tsx
+++ frontend/app.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
-import { Route, Switch } from 'react-router';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import { PageNotFound } from './components/commons/404PageNotFound';
 import { links } from './components/commons/linkUrls';
