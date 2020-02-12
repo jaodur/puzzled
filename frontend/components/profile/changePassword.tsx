6549aa9134d9778f2f25a9497e45e0a9a38d6942
odur <odurjoseph8@gmail.com>
2020-02-12 09:45:36 +0300

    ft: add change password functionality
     - add change password functionality
    
    [Delivers #170843732]

--- frontend/components/profile/changePassword.tsx
+++ frontend/components/profile/changePassword.tsx
@@ -1,19 +1,32 @@
 import * as React from 'react';
+import { useHistory } from 'react-router';
 
 import Typography from '@material-ui/core/Typography';
+import { isEqual } from 'lodash';
+import { useSnackbar } from 'notistack';
+import { useMutation } from 'react-apollo-hooks';
 
 import { validate } from 'validate.js';
+import { CHANGE_PASSWORD_MUTATION } from '../../graphql/mutations/authentication';
 import { deepCopy } from '../../utils/utils';
 import { Button } from '../commons/button';
+import { CustomSnackbarContentWrapper } from '../commons/customSnackbar';
 import { StackedInput } from '../commons/inputs';
+import { links } from '../commons/linkUrls';
+import { useCheckLoginContext } from '../commons/puzzleContext';
 import { EventInterface } from '../interfaces/interfaces';
 import { ChangePasswordInterface } from '../interfaces/profile';
 import { changePasswordConstraints } from '../validators/authentication';
 
 function ChangePassword({ styleClass, themeStyleClass }: ChangePasswordInterface) {
     const preventDefault = (event: any) => event.preventDefault();
+    const history = useHistory();
     const [password, setPassword] = React.useState(changePasswordInitState());
     const [passwordErrors, setPasswordErrors] = React.useState(changePasswordInitState());
+    // eslint-disable-next-line
+    const [changePasswordFunc, setChangePasswordFunc] = useMutation(CHANGE_PASSWORD_MUTATION);
+    const { asyncUpdateLoginInfo } = useCheckLoginContext();
+    const { enqueueSnackbar } = useSnackbar();
 
     function changePasswordInitState() {
         return {
@@ -50,6 +63,44 @@ function ChangePassword({ styleClass, themeStyleClass }: ChangePasswordInterface
         return !!value ? value[0] : '';
     }
 
+    function renderSnackbar(color: string) {
+        return function customSnackbar(key: any, message: string) {
+            return <CustomSnackbarContentWrapper id={key} message={message} color={color} />;
+        };
+    }
+
+    async function changePassword(event: EventInterface) {
+        preventDefault(event);
+
+        const errors = validateUserInputs(password, changePasswordConstraints);
+
+        if (!!errors) {
+            setPasswordErrors(errors || {});
+            return;
+        }
+
+        await changePasswordFunc({ variables: password })
+            .then(async () => {
+                await asyncUpdateLoginInfo(() => {});
+                enqueueSnackbar('Password changed successfully. Please re-login', {
+                    variant: 'success',
+                    content: renderSnackbar('success'),
+                });
+                history.push(links.USER.SIGN_IN);
+            })
+            .catch((response: any) => {
+                enqueueSnackbar(response.graphQLErrors[0].message, {
+                    variant: 'error',
+                    persist: true,
+                    content: renderSnackbar('secondary'),
+                });
+            });
+    }
+
+    function isCleanForm() {
+        return isEqual(changePasswordInitState(), password);
+    }
+
     return (
         <div>
             <div style={{ backgroundColor: 'white', padding: '5%' }}>
@@ -60,20 +111,20 @@ function ChangePassword({ styleClass, themeStyleClass }: ChangePasswordInterface
                 <div className={themeStyleClass.root}>
                     <StackedInput
                         label={'Current Password'}
-                        defaultValue={''}
+                        value={password.password}
                         type={'password'}
                         onChange={onPasswordFieldChange('password')}
                     />
                     <StackedInput
                         label={'New Password'}
-                        defaultValue={''}
+                        value={password.newPassword}
                         type={'password'}
                         onChange={onPasswordFieldChange('newPassword')}
                         errorMsg={checkEmpty(passwordErrors.newPassword)}
                     />
                     <StackedInput
                         label={'Confirm Password'}
-                        defaultValue={''}
+                        value={password.confirmPassword}
                         type={'password'}
                         onChange={onPasswordFieldChange('confirmPassword')}
                         errorMsg={checkEmpty(passwordErrors.confirmPassword)}
@@ -81,7 +132,12 @@ function ChangePassword({ styleClass, themeStyleClass }: ChangePasswordInterface
                 </div>
                 <div className={`${styleClass}__align_right`}>
                     <Button label={'cancel'} styleClass={'cancel-btn'} />
-                    <Button label={'set new password'} styleClass={'save-btn'} />
+                    <Button
+                        label={'set new password'}
+                        styleClass={'save-btn'}
+                        onBtnClick={changePassword}
+                        disabled={isCleanForm()}
+                    />
                 </div>
             </div>
         </div>
