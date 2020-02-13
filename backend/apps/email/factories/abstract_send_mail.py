19d43269d4e4cd2fda644ff261480470973ae6a1
odur <odurjoseph8@gmail.com>
2020-02-13 20:33:19 +0300

    ft: add additional abstract methods
     - add additional abstract methods
    
    [Delivers #170843727]

--- backend/apps/email/factories/abstract_send_mail.py
+++ backend/apps/email/factories/abstract_send_mail.py
@@ -3,8 +3,9 @@ import abc
 
 class AbstractEmail(metaclass=abc.ABCMeta):
 
+    @classmethod
     @abc.abstractmethod
-    def send_email(self, recipient, mail_subject, mail_body):
+    def send_email(cls, recipient, mail_subject, mail_body):
         """Abstract method for sending an email to a single recipient
 
         Args:
@@ -15,8 +16,9 @@ class AbstractEmail(metaclass=abc.ABCMeta):
         """
         pass
 
+    @classmethod
     @abc.abstractmethod
-    def send_bulk_emails(self, recipients, mail_subject, mail_body):
+    def send_bulk_emails(cls, recipients, mail_subject, mail_body):
         """Abstract method for sending email to multiple recipients
 
         Args:
@@ -24,5 +26,26 @@ class AbstractEmail(metaclass=abc.ABCMeta):
             mail_subject (str): the email subject.
             mail_body (str): the email body.
 
+        """
+        pass
+
+    @classmethod
+    @abc.abstractmethod
+    def send_multiple_emails(cls, *messages):
+        """Abstract method for sending multiple email messages to different recipients
+
+        Args:
+            messages (EmailMessage): a tuple of Django EmailMessage
+        """
+        pass
+
+    @classmethod
+    @abc.abstractmethod
+    def send_template_mail(cls, template, template_data):
+        """Abstract method for sending multiple email messages to different recipients
+
+        Args:
+            template (Undefined): a template to use for the email
+            template_data (dict): key-value pairs of data to populate the template
         """
         pass
