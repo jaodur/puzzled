b1e114e58a97bb3db044eb6b19e7591f7ab27e67
odur <odurjoseph8@gmail.com>
2020-02-15 22:01:04 +0300

    ft: overwrite the default call method on EmailSenderMeta
     - overwrite the default call method on EmailSenderMeta
    
    [Delivers #170843727]

--- backend/apps/email/factories/email_builder.py
+++ backend/apps/email/factories/email_builder.py
@@ -7,9 +7,10 @@ class EmailSenderMeta(type):
     def __new__(mcs, name, bases, namespaces, **kwargs):
 
         for attr_name, attr in namespaces.items():
-            if not issubclass(attr, AbstractEmail):
+
+            if callable(attr) and not issubclass(attr, AbstractEmail):
                 raise Exception(
-                    f'{attr_name} attribute must inherit from {AbstractEmail.__class__.__name__}'
+                    f'{attr_name} attribute must inherit from {AbstractEmail.__name__}'
                 )
 
         return super().__new__(mcs, name, bases, namespaces)
@@ -17,6 +18,23 @@ class EmailSenderMeta(type):
     def __init__(cls, name, bases, namespaces, **kwargs):
         # A do nothing __init__ to ensure both __new__ and __init__ have similar signature
         super().__init__(name, bases, namespaces)
+
+    def __call__(cls, *args, sender=None, **kwargs):
+
+        if args:
+            raise TypeError(f'Constructor for class {cls.__name__} does not accept positional arguments')
+
+        if sender:
+            klass = super().__call__(*args, **kwargs)
+
+            sender_class = getattr(klass, sender, False)
+
+            if not sender_class:
+                raise AttributeError(f'{klass.__name__} has no attribute {sender}')
+
+            return sender_class()
+
+        return super().__call__(*args, **kwargs)
 
 
 class EmailSender(metaclass=EmailSenderMeta):
