93273155411be1abc1c20fba15a5df219d9934fc
odur <odurjoseph8@gmail.com>
2020-02-17 23:51:11 +0300

    ft: add error_notification template
     -  add error_notification template
    
    [Delivers #170843727]

--- backend/apps/authentication/views.py
+++ backend/apps/authentication/views.py
@@ -1,10 +1,10 @@
 from django.conf import settings
-from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.core.signing import BadSignature, SignatureExpired
 from django.core.signing import loads as unsign
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.views.generic import View
+from backend.lib.urltools import make_absolute
 
 
 class VerifyEmail(View):
@@ -13,26 +13,31 @@ class VerifyEmail(View):
         try:
             data = unsign(signed_data, max_age=8 * 60 * 60)
         except SignatureExpired:
-            messages.error(self.request, 'Your email confirmation link has expired. You can try again.')
-            return redirect('/settings/personal/')
-        except BadSignature:
-            messages.error(self.request, 'This email confirmation link is invalid. Please try again.')
-            return redirect('/settings/personal/badSignature/')
+            message = 'Your email confirmation link has expired. Please generate new link.'
+            context = dict(message=message, redirect_url=make_absolute('/'))
+            return render(self.request, 'notifications/error_notification.html', context=context)
 
-        if request.user.is_anonymous:
+        except BadSignature:
+            message = 'This email confirmation link is invalid. Please try again.'
+            context = dict(message=message, redirect_url=make_absolute('/'))
+            return render(self.request, 'notifications/error_notification.html', context=context)
+
+        if self.request.user.is_anonymous:
             return redirect(settings.LOGIN_URL)
 
         if data['id'] != request.user.id:
-            messages.error(self.request, 'This email confirmation link is for a different account. Please try again.')
-            return redirect('/settings/personal/diff-ids/')
+            message = 'This email confirmation link is for a different account. Please try again.'
+            context = dict(message=message, redirect_url=make_absolute('/'))
+            return render(self.request, 'notifications/error_notification.html', context=context)
 
         old_email = self.request.user.email
         user_id = data['id']
         new_email = data['new_email']
 
         if old_email != new_email:
-            messages.error(self.request, 'The email being verified changed. Please regenerate confirmation link')
-            return redirect('/settings/personal/diff-email')
+            message = 'The email being verified changed. Please regenerate confirmation link'
+            context = dict(message=message, redirect_url=make_absolute('/'))
+            return render(self.request, 'notifications/error_notification.html', context=context)
 
         user = get_user_model().objects.get(id=user_id)
         user.email_verified = True
