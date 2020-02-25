354e5e739c4fbe2beb02958aee09e2973c54ae41
odur <odurjoseph8@gmail.com>
2020-02-25 19:29:42 +0300

    ft: test send_mail
     -  test send_mail
    
    [Delivers #170843727]

---
+++ backend/apps/email/tests/test_django_mail_sender.py
@@ -0,0 +1,24 @@
+from unittest.mock import patch
+from django.test import TestCase
+from django.core import mail
+from backend.apps.email.factories.django_mail import DjangoMailSender
+from .mocks import mock_django_q_async_send_message
+
+
+@patch('backend.apps.email.backends.django_q.async_task')
+class TestDjangoMailSender(TestCase):
+
+    def test_send_mail(self, mock_async_task):
+        mock_async_task.return_value = mock_django_q_async_send_message
+
+        recipient = 'test.email@example.com'
+        subject = 'test subject'
+        body = 'test body'
+
+        DjangoMailSender.send_email(recipient, subject, body)
+
+        email = mail.outbox[0]
+
+        self.assertEquals(subject, email.subject)
+        self.assertEquals(body, email.body)
+        self.assertEquals(recipient, email.recipients()[0])
