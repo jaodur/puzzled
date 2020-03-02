101f16cdd573773fb6f60afc0be6a8ecf7345e1e
odur <odurjoseph8@gmail.com>
2020-03-02 19:22:26 +0300

    ft: add chat models
     -  add chat models
    
    [Delivers #171567855]

--- backend/apps/chat/models.py
+++ backend/apps/chat/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from backend.lib.base import AuditableBaseModel
 
-# Create your models here.
+
+class Message(AuditableBaseModel):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    message = models.TextField()
+
+
+class Channel(AuditableBaseModel):
+    TYPE_CHOICES = (
+        ('Public', 'Public'),
+        ('Private', 'Private'),
+        ('Direct', 'Direct'),
+    )
+
+    name = models.CharField(max_length=50, null=False, blank=False, unique=True)
+    type = models.CharField(max_length=50, null=False, blank=False, choices=TYPE_CHOICES)
+    messages = models.ManyToManyField(Message)
+    users = models.ManyToManyField(get_user_model())
