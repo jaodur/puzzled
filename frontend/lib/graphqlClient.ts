2a8a6a00e634772393e5c5993cfd62cc98d16703
odur <odurjoseph8@gmail.com>
2020-03-06 20:00:14 +0300

    ft: add websocket link to apollo-client
     -  add websocket link to apollo-client
    
    [Delivers #171567855]

--- frontend/lib/graphqlClient.ts
+++ frontend/lib/graphqlClient.ts
@@ -1,15 +1,36 @@
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloClient } from 'apollo-client';
+import { ApolloLink } from 'apollo-link'
 import { createHttpLink } from 'apollo-link-http';
+import { WebSocketLink } from 'apollo-link-ws';
 
 import { stripTrailingSlash } from '../utils/utils';
+
+const hasSubscriptionOperation = ({ query: { definitions } }: any) =>
+  definitions.some(
+    ({ kind, operation }: { kind: string, operation: string }) =>
+      kind === 'OperationDefinition' && operation === 'subscription',
+  )
+
+const webSocketLink = new WebSocketLink({
+    uri: `${stripTrailingSlash(process.env.BASE_WS_URL)}/chat/`,
+    options: {
+        reconnect: true
+    }
+})
 
 const httpLink = createHttpLink({
     uri: `${stripTrailingSlash(process.env.BASE_URL)}/graphql/`,
 });
 
+const link = ApolloLink.split(
+    hasSubscriptionOperation,
+    webSocketLink,
+    httpLink,
+)
+
 const client = new ApolloClient({
-    link: httpLink,
+    link: link,
     cache: new InMemoryCache(),
 });
 
