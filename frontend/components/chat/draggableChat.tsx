c49123f964fc19f5a97c38f854d9a3bdfed811b4
ja-odur <odurjoseph8@gmail.com>
2020-04-01 16:32:32 +0300

    ft: style draggableChat component
     -  style draggableChat component
    
    [Delivers #171567855]

--- frontend/components/chat/draggableChat.tsx
+++ frontend/components/chat/draggableChat.tsx
@@ -1,12 +1,33 @@
 import * as React from 'react';
 
+import { makeStyles } from '@material-ui/core/styles';
+import ChatIcon from '@material-ui/icons/ChatOutlined';
+import CloseIcon from '@material-ui/icons/CloseOutlined';
+
 import { Draggable } from '../commons/draggable';
 import { ChatInterface, DraggableChatInterface } from '../interfaces/chat';
 
 const defaultChatStyleClass = 'default-draggable-chat';
 
+const useStyles = makeStyles({
+    root: {
+        fontSize: '32px',
+    },
+});
+
 function Chat({ styleClass }: ChatInterface) {
-    return <div className={styleClass || defaultChatStyleClass}>This is a test component</div>;
+    const classes = useStyles({});
+    return (
+        <div className={styleClass || defaultChatStyleClass}>
+            <div>
+                <span>
+                    <ChatIcon className={classes.root} />
+                    <span>chat</span>
+                    <CloseIcon className={classes.root} />
+                </span>
+            </div>
+        </div>
+    );
 }
 
 function DraggableChat({  }: DraggableChatInterface) {
