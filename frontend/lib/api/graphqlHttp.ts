d145fdef1b1103a2ce901ddc99817235e3f6332a
ja-odur <odurjoseph8@gmail.com>
2020-05-04 22:12:33 +0300

    ch: update graphqlHttp to include extraOptions
     - update graphqlHttp to include extraOptions
    
    [Maintains #172279704]

--- frontend/lib/api/graphqlHttp.ts
+++ frontend/lib/api/graphqlHttp.ts
@@ -1,13 +1,23 @@
 import graphqlClient from '../graphqlClient';
 
-async function graphqlMutate(mutation: any, variables: object = {}) {
-    return await graphqlClient.mutate({ mutation, variables }).then((response: any) => {
+type errorPolicyType = 'none' | 'ignore' | 'all';
+type fetchPolicyType = 'cache-first' | 'network-only' | 'cache-only' | 'no-cache' | 'standby';
+
+interface ExtraOptionsInterface {
+    errorPolicy?: errorPolicyType;
+    fetchPolicy?: fetchPolicyType;
+    optimisticResponse?: any;
+    refetchQueries?: any;
+}
+
+async function graphqlMutate(mutation: any, variables: object = {}, extraOptions?: ExtraOptionsInterface) {
+    return await graphqlClient.mutate({ mutation, variables, ...extraOptions }).then((response: any) => {
         return response.data;
     });
 }
 
-async function graphqlQuery(query: any, variables: object = {}) {
-    return await graphqlClient.query({ query, variables }).then((response: any) => {
+async function graphqlQuery(query: any, variables: object = {}, extraOptions?: ExtraOptionsInterface) {
+    return await graphqlClient.query({ query, variables, ...extraOptions }).then((response: any) => {
         return response.data;
     });
 }
