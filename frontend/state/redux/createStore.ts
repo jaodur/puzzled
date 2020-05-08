3aeb3121ff0c9c12cee845dbac6df4dd2acf1484
ja-odur <odurjoseph8@gmail.com>
2020-05-08 22:22:29 +0300

    ch: configure redux-saga
     - configure redux-saga
    
    [Maintains #172279704]

--- frontend/state/redux/createStore.ts
+++ frontend/state/redux/createStore.ts
@@ -1,13 +1,16 @@
 import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import createSagaMiddleware from 'redux-saga';
 import thunk from 'redux-thunk';
 
 import initialState from './initialState';
 import rootReducer from './reducers';
+import rootSaga from './sagas';
 
 export default function configureReduxStore() {
-    const middleware: any[] = [thunk];
+    const sagaMiddleware = createSagaMiddleware();
+    const middleware: any[] = [sagaMiddleware, thunk];
 
     // Be sure to ONLY add these middlewares in development!
     if (process.env.NODE_ENV !== 'production') {
@@ -17,6 +20,8 @@ export default function configureReduxStore() {
     const middlewareEnhancer = applyMiddleware(...middleware);
     const storeEnhancer = composeWithDevTools(middlewareEnhancer);
     const store = createStore(rootReducer, initialState, storeEnhancer);
+
+    sagaMiddleware.run(rootSaga);
 
     return store;
 }
