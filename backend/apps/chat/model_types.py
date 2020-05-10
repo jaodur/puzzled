e6ae5444c8ffa84988d178f9f967fbc3e012f3b7
ja-odur <odurjoseph8@gmail.com>
2020-05-10 11:58:28 +0300

    ch: add latest_message to channel model type
     - add latest_message to channel model type
    
    [Maintains #172279704]

--- backend/apps/chat/model_types.py
+++ backend/apps/chat/model_types.py
@@ -3,11 +3,6 @@ from graphene_django import DjangoObjectType
 from .models import ChatChannel, Message
 
 
-class ChatChannelModelType(DjangoObjectType):
-    class Meta:
-        model = ChatChannel
-
-
 class MessageModelType(DjangoObjectType):
     class Meta:
         model = Message
@@ -18,3 +13,13 @@ class MessageModelType(DjangoObjectType):
         if self.user == info.context.user:
             return 'right'
         return 'left'
+
+
+class ChatChannelModelType(DjangoObjectType):
+    class Meta:
+        model = ChatChannel
+
+    latest_message = graphene.Field(MessageModelType)
+
+    def resolve_latest_message(self, info):
+        return self.messages.last()
