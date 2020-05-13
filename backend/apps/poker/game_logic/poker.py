854645fed28bdc2deca65e32ced69db1cf0ca838
ja-odur <odurjoseph8@gmail.com>
2020-05-13 22:51:23 +0300

    ch: setup poker game structure
     - setup poker game structure
    
    [Finishes #172825140]

--- backend/apps/poker/game_logic/poker.py
+++ backend/apps/poker/game_logic/poker.py
@@ -6,6 +6,13 @@ class Hand:
         self.raw_hand = hand
         self.rank = ''
         self.suite = ''
+        self.name = ''
+
+    def sort_hand(self):
+        pass
+
+    def rank_hand(self):
+        pass
 
     def __repr__(self):
         return f'<Hand {self.raw_hand}>'
@@ -16,6 +23,18 @@ class Poker:
         self.deck_size = deck_size
         self.wild_cards = wild_cards
         self.hands = []
+
+    def shuffle_deck(self):
+        pass
+
+    def generate_hands(self, num_of_hands, hand_size):
+        pass
+
+    def sort_hands(self):
+        pass
+
+    def get_winning_hands(self):
+        pass
 
     def __iter__(self):
         return iter(self.hands)
