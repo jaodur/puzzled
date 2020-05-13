d70a0aad2928bd599c5a9e87a428f269d965a2a2
ja-odur <odurjoseph8@gmail.com>
2020-05-13 22:13:31 +0300

    ch: add poker class
     - add poker class
    
    [Finishes #172825140]

---
+++ backend/apps/poker/game_logic/poker.py
@@ -0,0 +1,23 @@
+from reprlib import repr
+
+
+class Poker:
+    def __init__(self, deck_size=1, wild_cards=False):
+        self.deck_size = deck_size
+        self.wild_cards = wild_cards
+        self.hands = []
+
+    def __iter__(self):
+        return iter(self.hands)
+
+    def __next__(self):
+        return next(self.hands)
+
+    def __len__(self):
+        return len(self.hands)
+
+    def __getitem__(self, index):
+        return self.hands[index]
+
+    def __repr__(self):
+        return f'<Poker hand_size={len(self)}, hands={repr(self.hands)}>'
