5002dae8a0576811533d79000abe678525acd722
ja-odur <odurjoseph8@gmail.com>
2020-05-19 23:10:00 +0300

    ch: add suites helper function
     - add suites helper function
    
    [Finishes #172825140]

--- backend/poker/game_logic/utils.py
+++ backend/poker/game_logic/utils.py
@@ -2,14 +2,29 @@ from enum import Enum
 
 
 class PokerSuites(Enum):
-    CLUBS = 'C'
-    HEARTS = 'H'
-    SPADES = 'S'
-    DIAMONDS = 'D'
+    CLUBS = 'C', 'c', 'clubs', '♣'
+    HEARTS = 'H', 'h', 'hearts', '♥'
+    SPADES = 'S', 's', 'spades', '♠'
+    DIAMONDS = 'D', 'd', 'diamonds', '♦'
 
     @classmethod
     def values(cls):
         return [e.value for e in cls]
+
+    @classmethod
+    def list_values(cls):
+        values = []
+        for e in cls:
+            values.extend(e.value)
+        return values
+
+    @classmethod
+    def get_mapper(cls):
+        mapper = {}
+        for e in cls:
+            val = e.value
+            mapper[val[0]] = val
+        return mapper
 
 
 class PokerHandValue(Enum):
