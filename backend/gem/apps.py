17d1cdcbdad31b96da52fed389af12febf341356
ja-odur <odurjoseph8@gmail.com>
2020-06-23 23:13:19 +0300

    ft: add gem signals
     - add gem signals
    
    [Finishes #173478211]

--- backend/gem/apps.py
+++ backend/gem/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class GemConfig(AppConfig):
     name = 'backend.gem'
     label = 'gem'
+
+    def ready(self):
+        import backend.gem.signals  # noqa: F401
---
+++ backend/gem/signals.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from .models import Gem
+
+
+def post_save_gem_handler(sender, instance, *args, created=False, **kwargs):
+
+    if created:
+        Gem(user=instance).save()
+
+
+post_save.connect(
+    post_save_gem_handler,
+    sender=get_user_model(),
+    dispatch_uid=f'{get_user_model().__class__.__name__}_gem_signal'
+)
