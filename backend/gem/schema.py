86e1557dc1765d59268a45bafe41a87a75f9ebfa
ja-odur <odurjoseph8@gmail.com>
2020-06-23 23:42:00 +0300

    ft: add gem schema
     - add gem schema
    
    [Finishes #173478211]

---
+++ backend/gem/schema.py
@@ -0,0 +1,28 @@
+import graphene
+from graphene_django import DjangoObjectType
+from backend.lib.decorators.permissions import is_authenticated
+from backend.lib.exceptions import FieldValidationError
+from .models import Gem
+
+
+class GemModelType(DjangoObjectType):
+    class Meta:
+        model = Gem
+
+
+class GemQuery(graphene.ObjectType):
+    gem = graphene.Field(GemModelType, user=graphene.String())
+
+    @is_authenticated
+    def resolve_gem(self, info, user):
+        gem = Gem.objects.get(user=user)
+
+        if gem.user == info.context.user:
+            return gem
+
+        raise FieldValidationError(
+            'You do not have the required permission to view this field',
+            field=None,
+            params=None,
+            code='Invalid'
+        )
--- backend/schema.py
+++ backend/schema.py
@@ -1,14 +1,15 @@
 import graphene
-from backend.sudoku.schema import SudokuMutation, SudokuQuery
 from backend.authentication.schema import UserMutation, UserQuery
 from backend.chat.schema import ChatMutations, ChatSubscriptions
+from backend.gem.schema import GemQuery
+from backend.sudoku.schema import SudokuMutation, SudokuQuery
 
 
 class Mutation(UserMutation, SudokuMutation, ChatMutations, graphene.ObjectType):
     pass
 
 
-class Query(UserQuery, SudokuQuery, graphene.ObjectType):
+class Query(UserQuery, SudokuQuery, GemQuery, graphene.ObjectType):
     pass
 
 
