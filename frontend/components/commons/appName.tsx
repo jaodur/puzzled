6fcd83238c887a6ee78830fcc4c11895b14f2ac2
ja-odur <odurjoseph8@gmail.com>
2020-06-27 22:08:41 +0300

    ft: add responsive meta data in page header
     - add responsive meta data in page header
    
    [Finishes #173499687]

--- frontend/components/commons/appName.tsx
+++ frontend/components/commons/appName.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 function AppName() {
     return (
-        <div className="appName">
+        <div className="appSlogan">
             <h1>Make games a little more fun, in more ways than ever.</h1>
         </div>
     );
