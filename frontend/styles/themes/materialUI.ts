714d09356c6d1337d5021fadff5a16e46412923d
ja-odur <odurjoseph8@gmail.com>
2020-07-08 18:49:42 +0300

    ft: change material ui palette primary color
     - change material ui palette primary color
    
    [Finishes #173694633]

--- frontend/styles/themes/materialUI.ts
+++ frontend/styles/themes/materialUI.ts
@@ -4,6 +4,11 @@ const materialUITheme = createMuiTheme({
     typography: {
         fontFamily: ['Source Sans Pro', 'sans-serif'].join(','),
     },
+    palette: {
+        primary: {
+            main: '#2662ac'
+        }
+    }
 });
 
 export { materialUITheme };
