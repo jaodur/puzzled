fe2d99959fc71d82df61ceb7376674f3579c017e
ja-odur <odurjoseph8@gmail.com>
2020-07-09 20:23:45 +0300

    ch: add responsive design
     - add responsive design
    
    [Finishes #173729640]

--- frontend/components/commons/footer.tsx
+++ frontend/components/commons/footer.tsx
@@ -5,32 +5,32 @@ function Footer({ className }: FooterInterface) {
     className = className ? className : 'footer-main';
     return (
         <footer className={className}>
-            <div className={`${className}__content`}>
-                <div className={`${className}__content__logo`}>
+            <div className={'footer-content'}>
+                <div className={'footer-logo'}>
                     <h1>puzzled</h1>
                 </div>
-                <div className={`${className}__content__card`}>
+                <div className={'footer-card'}>
                     <h1>puzzled</h1>
                     <p>about</p>
                     <p>blogs</p>
                     <p>puzzled premium</p>
                     <p>contact us</p>
                 </div>
-                <div className={`${className}__content__card`}>
+                <div className={'footer-card'}>
                     <h1>games</h1>
                     <p>Sudoku</p>
                     <p>Mine sweeper</p>
                     <p>Poker</p>
                     <p>2048</p>
                 </div>
-                <div className={`${className}__content__card`}>
+                <div className={'footer-card'}>
                     <h1>leaderboards</h1>
                     <p>Sudoku</p>
                     <p>Mine sweeper</p>
                     <p>Poker</p>
                     <p>2048</p>
                 </div>
-                <div className={`${className}__content__card`}>
+                <div className={'footer-card'}>
                     <h1>extras</h1>
                     <p>Sudoku</p>
                     <p>Mine sweeper</p>
@@ -38,7 +38,7 @@ function Footer({ className }: FooterInterface) {
                     <p>2048</p>
                 </div>
             </div>
-            <div className={`${className}__copyright`}>
+            <div className={'footer-copyright'}>
                 <p>© {new Date().getFullYear()} Puzzled</p>
                 <span>
                     <a href="#">Terms</a>
