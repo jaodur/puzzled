c2e8a4efe13aa1807c6137f1bbcaa7f128757696
ja-odur <odurjoseph8@gmail.com>
2020-07-17 14:09:00 +0300

    ch: add seat avatars to poker table
     - add seat avatars to poker table
    
    [Finishes #173749690]

--- frontend/components/poker/pokerTable.tsx
+++ frontend/components/poker/pokerTable.tsx
@@ -1,10 +1,64 @@
 import * as React from 'react';
 
+import { Card } from '../icons/cards';
+import { PokerAvatar } from './pokerAvatar';
+
 function PokerTable() {
     return (
-        <div className={'poker-table'}>
-            <div className={'p-table-content'} />
-        </div>
+        <>
+            <div className={'poker-table'}>
+                <div className={'p-table-content'}>
+                    <Card value={'TH'} />
+                    <Card value={'JH'} />
+                    <Card value={'QH'} />
+                    <Card value={'KH'} />
+                    <Card value={'AH'} />
+                </div>
+                <PokerAvatar
+                    className={'t-avatar av-bnl'}
+                    tiltClass={'t-bnl'}
+                    profileName={'ja-odur'}
+                    src={'https://i.pravatar.cc/150?img=3'}
+                />
+                <PokerAvatar
+                    className={'t-avatar av-bt'}
+                    tiltClass={'t-bt'}
+                    profileName={'ja-odur'}
+                    src={'https://i.pravatar.cc/150?img=3'}
+                />
+                <PokerAvatar
+                    className={'t-avatar av-bnr'}
+                    tiltClass={'t-bnr'}
+                    profileName={'ja-odur'}
+                    src={'https://i.pravatar.cc/150?img=3'}
+                />
+                <PokerAvatar
+                    className={'t-avatar av-tnl'}
+                    tiltClass={'t-tnl'}
+                    profileName={'ja-odur'}
+                    src={'https://i.pravatar.cc/150?img=3'}
+                />
+                <PokerAvatar
+                    className={'t-avatar av-tp'}
+                    tiltClass={'t-tp'}
+                    profileName={'ja-odur'}
+                    src={'https://i.pravatar.cc/150?img=3'}
+                />
+                <PokerAvatar
+                    className={'t-avatar av-tnr'}
+                    tiltClass={'t-tnr'}
+                    profileName={'ja-odur'}
+                    src={'https://i.pravatar.cc/150?img=3'}
+                />
+                <PokerAvatar
+                    className={'t-avatar av-frt'}
+                    tiltClass={'t-frt'}
+                    profileName={'ja-odur'}
+                    src={'https://i.pravatar.cc/150?img=3'}
+                />
+                <PokerAvatar className={'t-avatar av-flb'} tiltClass={'t-flb'} profileName={'ja-odur'} src={''} />
+            </div>
+        </>
     );
 }
 
