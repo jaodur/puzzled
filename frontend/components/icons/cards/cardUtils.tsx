d1acbd9e38bb2871eedcefff536c3ad0bc5771d4
ja-odur <odurjoseph8@gmail.com>
2020-07-18 19:52:11 +0300

    ch: remove default tilt class from card-util
     - remove default tilt class from card-util
    
    [Finishes #173749690]

--- frontend/components/icons/cards/cardUtils.tsx
+++ frontend/components/icons/cards/cardUtils.tsx
@@ -4,7 +4,7 @@ import { ElementInterface } from '../../interfaces/interfaces';
 
 function Tilt({ className, children, style }: ElementInterface) {
     return (
-        <div className={`card-tilt ${className}`} style={style || {}}>
+        <div className={className} style={style || {}}>
             {children}
         </div>
     );
