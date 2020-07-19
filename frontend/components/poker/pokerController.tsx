0965750d28ffc0fb152b8c42d76f95fbe84a1fd5
ja-odur <odurjoseph8@gmail.com>
2020-07-19 23:31:22 +0300

    ch: add raise slider to table
     - add raise slider to table
    
    [Finishes #173749690]

--- frontend/components/poker/pokerController.tsx
+++ frontend/components/poker/pokerController.tsx
@@ -1,15 +1,18 @@
 import * as React from 'react';
 
 import { Button } from '@material-ui/core';
+
+import { PrettoInputSlider } from '../commons/slider';
 import { Card, Tilt } from '../icons/cards';
 
 function PokerController() {
     return (
         <div className={'p-game-controller'}>
             <div>
+                <PrettoInputSlider value={20} min={-20} max={200} />
                 <Button>Raise</Button>
             </div>
-            <div>
+            <div className={'p-button-c'}>
                 <Button color={'secondary'}>Fold</Button>
                 <Button color={'secondary'}>Leave Room</Button>
             </div>
@@ -17,7 +20,7 @@ function PokerController() {
                 <Card value={'AH'} size={'large'} />
                 <Card value={'KH'} size={'large'} />
             </Tilt>
-            <div>
+            <div className={'p-button-c'}>
                 <Button color={'primary'}>Call/Check</Button>
             </div>
         </div>
