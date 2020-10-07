2615142df5e18866d8e6ee026f290070a5a99500
ja-odur <odurjoseph8@gmail.com>
2020-10-07 22:29:21 +0300

    ft: redesign app slogan
     - redesign app slogan
    
    [Finishes #174018605]

--- frontend/components/commons/appSlogan.tsx
+++ frontend/components/commons/appSlogan.tsx
@@ -1,7 +1,14 @@
 import * as React from 'react';
 
 function AppSlogan() {
-    return <div className="app-slogan">Make games a little more fun, in more ways than ever.</div>;
+    return (
+        <div className="app-slogan">
+            <div>
+                <span>unlock the <span>magic</span></span>
+                <span>of gaming</span>
+            </div>
+        </div>
+    );
 }
 
 export { AppSlogan };
